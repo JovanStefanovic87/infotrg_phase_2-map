27e1374fce27fbaf4dd5199bddac4a345120a7ef
JovanStefanovic87 <zbni.rs@gmail.com>
2024-11-24 19:01:34 +0100

    Header logo fix

--- app/components/buttons/LogoButton.tsx
+++ app/components/buttons/LogoButton.tsx
@@ -6,7 +6,16 @@ const LogoButton: React.FC = () => {
 	return (
 		<Link href='/' passHref>
 			<div className='flex justify-center items-center w-full h-full rounded-lg overflow-hidden sm:mr-8'>
-				<Image src={logoMini} alt='Button Icon' width={40} height={40} priority quality={100} />
+				<Image
+					src={logoMini}
+					alt='Button Icon'
+					width={45}
+					height={45}
+					priority
+					quality={100}
+					style={{ height: '45px', width: '45px' }}
+					className='object-contain'
+				/>
 			</div>
 		</Link>
 	);
